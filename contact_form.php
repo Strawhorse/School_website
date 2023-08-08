2ff8184a35e902f9aa7466d81e70198b2c400313
Sean B <strawhorse@gmail.com>
2023-08-08 20:45:58 +0100

    changing form name values

--- contact_form.php
+++ contact_form.php
@@ -9,6 +9,37 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     require 'includes/database.php';
 
     $sql_query = "INSERT INTO queries (person_name, email,	telephone, query, contact_message) VALUES (?,?,?,?,?)";
+
+
+
+    // create a prepared statement instead
+    $stmt = mysqli_prepare($conn,$sql_query);
+
+    if ($stmt === false) {
+
+        echo mysqli_error($conn);
+
+    } else {
+
+
+        mysqli_stmt_bind_param($stmt, 'ssiss', $_POST['person_name'], $_POST['email'], $_POST['telephone'], $_POST['query'],$_POST['contact_message']);
+
+        
+
+
+        if(mysqli_stmt_execute($stmt)) {
+
+            // function picks up the ID from the last insert
+            $id = mysqli_insert_id($conn);
+            echo "Query created with record number: $id";
+
+        } else {
+            echo mysqli_stmt_error($stmt);
+            
+        }
+
+    }
+
 }
 
 
