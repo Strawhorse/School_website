08037f252b79d5b9504abbab614325038065a8ba
Sean B <strawhorse@gmail.com>
2023-08-08 20:52:30 +0100

    php file not running, will try to use code from college assignment tomorrow

--- contact_form.php
+++ contact_form.php
@@ -8,7 +8,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     require 'includes/database.php';
 
-    $sql_query = "INSERT INTO queries (person_name, email,	telephone, query, contact_message) VALUES (?,?,?,?,?)";
+    $sql_query = "INSERT INTO queries (person_name, email, telephone, query, contact_message) VALUES (?,?,?,?,?)";
 
 
 
