a8a2db9469cd2987b2ec2436e54264076bf72a88
Sean B <strawhorse@gmail.com>
2023-08-08 20:34:00 +0100

    Writing php query to post to database the user messages

--- contact_form.php
+++ contact_form.php
@@ -1,2 +1,20 @@
-<!-- .n1WIfjzFEoi0NKA -->
+<?php
+
+
+// check to make sure html form action is post
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+    require 'includes/database.php';
+
+    $sql_query = "INSERT INTO queries (person_name, email,	telephone, query, contact_message) VALUES (?,?,?,?,?)";
+}
+
+
+
+
+
+?>
+
 
---
+++ includes/database.php
@@ -0,0 +1,17 @@
+<?php
+
+
+$db_host = "localhost";
+$db_name = "school";
+$db_user = "management";
+$db_pass = ".n1WIfjzFEoi0NKA";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit;
+}
+
+
+?>
