5fd0790cb32f39d4debd4c3fa720672c5e80ae82
Sean B <strawhorse@gmail.com>
2023-08-09 20:00:17 +0100

    Telephone reentered as double instead of int

--- contact_form.php
+++ contact_form.php
@@ -8,7 +8,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     require 'includes/database.php';
 
-    $sql_query = "INSERT INTO queries (person_name, email, query, contact_message) VALUES (?,?,?,?)";
+    $sql_query = "INSERT INTO queries (person_name, email, telephone, query, contact_message) VALUES (?,?,?,?,?)";
 
 
 
@@ -22,7 +22,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
 
 
-        mysqli_stmt_bind_param($stmt, 'ssss', $_POST['person_name'], $_POST['email'], $_POST['query'],$_POST['contact_message']);
+        mysqli_stmt_bind_param($stmt, 'ssdss', $_POST['person_name'], $_POST['email'], $_POST['telephone'], $_POST['query'],$_POST['contact_message']);
 
         
 
@@ -31,7 +31,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
             // function picks up the ID from the last insert
             $id = mysqli_insert_id($conn);
-            echo "Query created with record number: $id";
+            echo "<h1>Query created with record number: $id<br>";
+            echo "<h2>Booking stored successfully.<br>";
+            echo "<h2>Now returning you to the main school page. Please wait ...";
+            header("refresh:3;url=index.html");
 
         } else {
             echo mysqli_stmt_error($stmt);
@@ -42,8 +45,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 }
 
-
-
+// close connection
+$conn->close();
 
 
 ?>
--- includes/database.php
+++ includes/database.php
@@ -9,8 +9,9 @@ $db_pass = "";
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
-    exit;
+    // echo mysqli_connect_error();
+    // exit;
+    die("Message sending failed: " . $conn->connect_error);
 }
 
 
