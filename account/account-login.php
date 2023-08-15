f986a9eb8e5169a4c9764b0db09ba1a8e4f63743
Sean B <strawhorse@gmail.com>
2023-08-15 15:52:37 +0100

    Update account-login.php

--- account/account-login.php
+++ account/account-login.php
@@ -4,9 +4,11 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if($_POST['email'] == 'management@gmail.com' && $_POST['password'] == 'ishu2Yaa') {
-        die("login correct");
+        $_SESSION['is_logged_in'] = true;
+        echo "<h1>login correct, redirecting ...<?h1>";
+        header("refresh:3;url=account.html");
     } else{
         die("Incorrect login!");
-        
+
     }
 }
