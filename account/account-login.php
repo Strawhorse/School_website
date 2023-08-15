479807f7b95b6f7f1dfa02b58e01f571ce3dec73
Sean B <strawhorse@gmail.com>
2023-08-15 15:44:56 +0100

    Trying login script

--- account/account-login.php
+++ account/account-login.php
@@ -2,6 +2,11 @@
 
 session_start();
 
-$_SESSION['is_logged_in'] = true;
-
-var_dump($_SESSION);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if($_POST['email'] == 'management@gmail.com' && $_POST['password'] == 'ishu2Yaa') {
+        die("login correct");
+    } else{
+        die("Incorrect login!");
+        
+    }
+}
--- account/account-logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-session_start();
-
-$_SESSION['is_logged_in'] = false;
-
-var_dump($_SESSION);
