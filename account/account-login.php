18217f21a2eac87bf9a289952d8f232a6d889289
Sean B <strawhorse@gmail.com>
2023-08-16 20:19:47 +0100

    Updating php login script

--- account/account-login.php
+++ account/account-login.php
@@ -1,5 +1,8 @@
 <?php
 
+// include '../includes/header.php';
+// include '../includes/footer.php';
+
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -9,8 +12,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         session_regenerate_id(true);
                 
         $_SESSION['is_logged_in'] = true;
+
+        require '../includes/header.php';
         echo "<h1>login correct, redirecting ...<?h1>";
         header("refresh:3;url=account.html");
+        require '../includes/footer.php';
+
+    } elseif ($_POST['email'] == 'staff@gmail.com' && $_POST['password'] == 'ishu2Yaa-'){
+        // regenerate session after logging in
+        session_regenerate_id(true);
+                
+        $_SESSION['is_logged_in'] = true;
+
+
+        require '../includes/header.php';
+        echo "<h1>login correct, redirecting ...<?h1>";
+        header("refresh:3;url=account.html");
+        require '../includes/footer.php';
     } else{
         die("Incorrect login!");
 
