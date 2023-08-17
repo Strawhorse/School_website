d6d821fbe0da83653d0a80798e270313f3313413
Sean B <strawhorse@gmail.com>
2023-08-17 20:10:33 +0100

    Small fixes

--- account/account-login.php
+++ account/account-login.php
@@ -5,8 +5,10 @@
 
 session_start();
 
+$username = $_POST["email"];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if($_POST['email'] == 'management@gmail.com' && $_POST['password'] == 'ishu2Yaa') {
+    if($_POST['email'] == 'management@gmail.com' && $_POST['password'] == 'aaa') {
         
         // regenerate session after logging in
         session_regenerate_id(true);
@@ -14,11 +16,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $_SESSION['is_logged_in'] = true;
 
         require '../includes/header.php';
-        echo "<h1>login correct, redirecting ...<?h1>";
+        
+        echo "<h1>Login correct, $username <br> Welcome back, redirecting ...<?h1>";
         header("refresh:3;url=account.html");
         require '../includes/footer.php';
 
-    } elseif ($_POST['email'] == 'staff@gmail.com' && $_POST['password'] == 'ishu2Yaa-'){
+    } elseif ($_POST['email'] == 'staff@gmail.com' && $_POST['password'] == 'aaa-'){
         // regenerate session after logging in
         session_regenerate_id(true);
                 
@@ -26,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
         require '../includes/header.php';
-        echo "<h1>login correct, redirecting ...<?h1>";
+        echo "<h1>Login correct, $username <br> Welcome back, redirecting ...<?h1>";
         header("refresh:3;url=account.html");
         require '../includes/footer.php';
     } else{
