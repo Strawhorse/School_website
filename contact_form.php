f0d17cdac3fdd2874d93ef02c7eda8f6ee963656
Sean B <strawhorse@gmail.com>
2023-08-23 16:44:53 +0100

    Changed structure and tidied up prepared statement in query file

--- contact_form.php
+++ contact_form.php
@@ -21,57 +21,49 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $conn = $db->getConn();
 
-    $prep_statement = "INSERT INTO queries (person_name, email, telephone, contact_message) VALUES (:person_name, :email, :telephone, :contact_message)";
+    // $prep_statement = "INSERT INTO queries (person_name, email, telephone, contact_message) VALUES (:person_name, :email, :telephone, :contact_message)";
 
 
-    if ($prep_statement === false) {
+    $query = $conn -> prepare('INSERT INTO queries (person_name, email, telephone, contact_message) VALUES (:person_name, :email, :telephone, :contact_message)');
 
-        echo $conn->errorInfo();
+    $query->bindParam(':person_name', $person_name, PDO::PARAM_STR);
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->bindParam(':telephone', $telephone, PDO::PARAM_INT);
+    // $query->bindParam(':query', $query,PDO::PARAM_STR);
+    $query->bindParam(':contact_message', $contact_message, PDO::PARAM_STR);
 
-    } else {
 
-        $query = $conn -> prepare($prep_statement);
+    if($query->execute()) {
 
-        $query->bindParam(':person_name', $person_name, PDO::PARAM_STR);
-        $query->bindParam(':email', $email, PDO::PARAM_STR);
-        $query->bindParam(':telephone', $telephone, PDO::PARAM_INT);
-        // $query->bindParam(':query', $query,PDO::PARAM_STR);
-        $query->bindParam(':contact_message', $contact_message, PDO::PARAM_STR);
+        // function picks up the ID from the last insert
+        $id = $conn->lastInsertId();
 
- 
-        if($query->execute()) {
+        require 'includes/header.php';
 
-            // function picks up the ID from the last insert
-            $id = $conn->lastInsertId();
+        echo "The URI of the page sending this request is" . $_SERVER['REQUEST_URI'];
 
-            require 'includes/header.php';
-
-            echo "The URI of the page sending this request is" . $_SERVER['REQUEST_URI'];
-
-            echo "<h3>Query created with record number: $id<br>";
-            echo "<h3>Booking stored successfully.<br>";
-            echo "<h3>Now returning you to the main school page. Please wait ...";
-            
-            if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
-                $protocol = 'https';
-            } else{
-                $protocol = 'http';
-            }
-
-            $server_name = $_SERVER['HTTP_HOST'];
-
-            $total_URL = $protocol . "://" . $server_name . "/School_website/contact.html";
-            
-            header("refresh:4;url= $total_URL");
-
-            require 'includes/footer.php';
-
-        } else {
-            echo "Something happened, error details below:";
-            echo $conn->errorInfo();
-            
+        echo "<h3>Query created with record number: $id<br>";
+        echo "<h3>Booking stored successfully.<br>";
+        echo "<h3>Now returning you to the main school page. Please wait ...";
+        
+        if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+            $protocol = 'https';
+        } else{
+            $protocol = 'http';
         }
 
+        $server_name = $_SERVER['HTTP_HOST'];
+
+        $total_URL = $protocol . "://" . $server_name . "/School_website/contact.html";
+        
+        header("refresh:4;url= $total_URL");
+
+        require 'includes/footer.php';
+
+    } else {
+        echo "Something happened, error details below:";
+        echo $conn->errorInfo();
+        
     }
 
 }
