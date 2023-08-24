f7caeded952db1e0e9e91c597512b9ff62001a2b
Sean B <strawhorse@gmail.com>
2023-08-24 20:49:49 +0100

    created new function to return a query as an object

--- classes/Query.php
+++ classes/Query.php
@@ -2,6 +2,14 @@
 
 class Query{
 
+    // public properties of a query to turn it into an object
+
+    public $id;
+    public $person_name;
+    public $email;
+    public $telephone;
+    public $contact_message;
+
 
     // create function to get all articles to display on Account
 
@@ -14,4 +22,21 @@ class Query{
         
 
     }
+
+    // function to return a query as an object
+
+    public static function getByID($conn, $id, $columns = '*') {
+        $sql = "SELECT $columns FROM queries WHERE id = :id";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'Query');
+
+        if($stmt->execute()) {
+            return $stmt->fetch();
+        }
+    }
+
+
 }
