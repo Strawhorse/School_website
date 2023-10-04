319f71fc56cce23853402fdcfd38cdc1e82c2471
Sean B <strawhorse@gmail.com>
2023-10-04 19:50:12 +0100

    student log in and log out working

---
+++ student_account/account-logout.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once dirname(__DIR__) . '/classes/Auth.php';
+
+session_start();
+
+Auth::logout();
+
+// $_SESSION['is_logged_in'] = false;
+echo "<h1>Logged out, redirecting ...<?h1>";
+header("refresh:3;url=../student_login.html");
+
+
+// alternatively to destroy a session
+
